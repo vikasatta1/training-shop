3f36f3425b34e290a41dddd40c792ffd3983fc2d
Vika <Vikasatta1998@gmail.com>
2022-02-06 14:22:03 +0300

    main header2

--- src/index.js
+++ src/index.js
@@ -8,16 +8,16 @@ import {BrowserRouter} from "react-router-dom";
 
 WebFont.load({
     google: {
-        families: ['Poppins', "Spartan:500","Inter:400"]
+        families: ['Poppins', "Spartan:500", "Inter:400"]
     }
 });
 ReactDOM.render(
-  <React.StrictMode>
-      <BrowserRouter>
-    <App />
-      </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
