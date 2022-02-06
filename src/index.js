c4a4d6dc779db1f06e4d7cce8f38a13cc005e295
Vika <Vikasatta1998@gmail.com>
2022-02-06 14:24:00 +0300

    Revert "main header"
    This reverts commit ce74a3fe

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import WebFont from "webfontloader";
-import {BrowserRouter} from "react-router-dom";
 
 WebFont.load({
     google: {
@@ -13,9 +12,7 @@ WebFont.load({
 });
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter>
     <App />
-      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
