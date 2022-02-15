97ec020c58e96e38fcb30aaa75936029cc445b21
Vika <Vikasatta1998@gmail.com>
2022-02-15 12:39:37 +0300

    clothes route

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import './App.scss';
 import MainPage from "./pages/main-page/main-page";
 import Header from "./components/header/heade";
 import Footer from "./components/footer/footer";
-import {Routes,Route} from "react-router-dom";
-
+import {Routes ,Route} from "react-router-dom";
 import './App.scss'
+
+import Clothes from "./components/main-blocks/clothes/clothes";
 export const App = () => {
     return (
         <div className='app' data-test-id='app'>
             <Header/>
             <Routes>
-                <Route path={'/'} element={<MainPage/>}/>
+
+                <Route exact path={'/'} element={<MainPage/>}/>
+                <Route path={'/women'} element={ <Clothes productType='women' />}/>
+                <Route  path={'/men'} element={<Clothes productType='men' />}/>
             </Routes>
+
 
             <Footer/>
         </div>
